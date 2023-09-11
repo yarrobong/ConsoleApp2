6e5a8ff39bd6548d96d48cd12ca9f2bf2bab8bd0
yarrobong <oneboardsholi@gmail.com>
2023-09-11 21:50:56 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, создатель, как тебя зовут?");
+            string answerUser = Console.ReadLine();
+
+            Console.WriteLine("А как ты назовешь меня?");
+            string nameBot = Console.ReadLine();
+            Console.WriteLine($"Приятно познакомиться, {answerUser}. Меня зовут {nameBot}, это ты меня так назвал");
+        }
+    }
+}
